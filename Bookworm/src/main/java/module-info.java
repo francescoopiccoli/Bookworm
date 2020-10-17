c237aa00995993251e70c6f013312062b370ea5c
Francesco Piccoli <70111810+francescoopiccoli@users.noreply.github.com>
2020-10-17 16:20:22 +0200

    Implementing google book java library

--- Bookworm/src/main/java/module-info.java
+++ Bookworm/src/main/java/module-info.java
@@ -1,4 +1,8 @@
 module bookworm.Bookworm {
     requires javafx.controls;
+	requires google.api.client;
+	requires com.google.api.client;
+	requires com.google.api.client.json.jackson2;
+	requires google.api.services.books.v1.rev114;
     exports com.Bookworm;
 }
