62d640ad3aa93bf74b57d83a26cf5cac82b9ce55
deiv24 <35795745+deiv24@users.noreply.github.com>
2020-10-20 15:45:40 +0200

    Add files via upload

---
+++ Bookworm/src/main/java/com/Bookworm/ui/Disc/Discover.java
@@ -0,0 +1,82 @@
+package Disc;
+
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+import java.io.FileNotFoundException;
+
+public class Discover {
+    public Node getCenterDisc() throws FileNotFoundException {
+        //Temporary image to replace covers
+        Image image = new Image("Disc/300.png");
+        //Create vertical box will align all elements one under the other
+        VBox vb = new VBox();
+
+        vb.getStyleClass().add("vbrect");
+        vb.setSpacing(50);
+        //Create horizontal box will align all elements one next to the other
+        HBox hb = new HBox();
+
+        //Button for all single books to be created
+        Button rect = new Button();
+
+
+        //Add a scroll pane so scrolling is made possible
+        ScrollPane scrollPane;
+
+
+        Label label  = new Label("Discover new books!");
+        label.getStyleClass().add("discoverLabel");
+        vb.getChildren().add(label);
+        //will create (currently 3) rows with at most 5 books on each row
+        for (int i = 0; i<3;i++){
+            hb = new HBox();
+
+            for (int j = 0; j<5;j++){
+                //This will be replaced with the function giving us the cover of the book and also setting the reaction to clicking the "button"
+                ImageView imageView = new ImageView(image);
+                rect = new Button("Title placeholder",imageView);
+                rect.getStyleClass().add("rect");
+                hb.getChildren().add(rect);
+
+            }
+            vb.getChildren().add(hb);
+        }
+        scrollPane = new ScrollPane(vb);
+        scrollPane.getStyleClass().add("scrollpane");
+
+        return  scrollPane;
+
+
+
+    }
+
+    public Node createTopDisc() {
+        VBox vb = new VBox();
+        HBox hb = new HBox();
+        //VBox v = new VBox();
+
+
+        TextField search = new TextField();
+        TextField filters = new TextField();
+
+
+        Button apply = new Button("Filter");
+        apply.setOnAction(event -> {filter();});
+        hb.getChildren().addAll(filters,apply);
+        vb.getChildren().addAll(search,hb);
+        vb.setSpacing(5.5);
+        return  vb;
+    }
+    private void filter() {
+
+    }
+
+}
---
+++ Bookworm/src/main/java/com/Bookworm/ui/Disc/DiscoverStart.java
@@ -0,0 +1,45 @@
+package Disc;
+
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class DiscoverStart extends Application {
+    Stage window;
+    Scene scene;
+    BorderPane layout;
+    ScrollPane scrollPane;
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+        window = primaryStage;
+        //Since discover is first scene show that first and set it as initial scene; layout is the current discover scene.
+        primaryStage.setTitle("Bookworm - Discover");
+        layout = new BorderPane();
+        //Create an instance of Discover to fill the borderpane with its functions
+        Discover disc = new Discover();
+        layout.setTop(disc.createTopDisc());
+        layout.setCenter(disc.getCenterDisc());
+        scene = new Scene(layout, 1800,750);
+        //Sets css to the discover scene
+        scene.getStylesheets().add("Disc/style.css");
+        window.setScene(scene);
+        //Other characteristics for window of the application.
+        window.setResizable(false);
+        window.show();
+        primaryStage.show();
+
+
+
+    }
+
+
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
