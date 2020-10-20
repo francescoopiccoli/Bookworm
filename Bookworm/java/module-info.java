9e8178bc104a08e73f7f07bfb03439b6ad1fefb3
cunidev <raffaele.tranquillini@gmail.com>
2020-10-20 10:21:41 +0300

    regenerate project structure

--- Bookworm/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.Bookworm {
-    requires javafx.controls;
-	requires google.api.client;
-	requires com.google.api.client;
-	requires com.google.api.client.json.jackson2;
-	requires google.api.services.books.v1.rev114;
-	requires javafx.graphics;
-    exports com.Bookworm;
-}
