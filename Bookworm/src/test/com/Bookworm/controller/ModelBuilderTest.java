45d90d3a68b5b3a641c9be965f258a21e2113486
mattf13 <63061267+mattf13@users.noreply.github.com>
2020-11-08 18:32:12 +0100

    Added some tests for tags and bookshelf, removed useless column

--- Bookworm/src/test/com/Bookworm/controller/ModelBuilderTest.java
+++ Bookworm/src/test/com/Bookworm/controller/ModelBuilderTest.java
@@ -1,9 +1,15 @@
 package com.Bookworm.controller;
 
 import com.Bookworm.model.Book;
+import com.Bookworm.model.Bookshelf;
+import com.Bookworm.model.Tag;
 import org.junit.jupiter.api.Test;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.fail;
 
 
 class ModelBuilderTest {
@@ -34,11 +40,19 @@ class ModelBuilderTest {
     }
 
     @Test
-    void makeBookshelves() {
+    void makeBookshelves() throws SQLException, ClassNotFoundException {
+       List<Bookshelf> bookshelfList =dbtest.getBookShelves();
+        if (bookshelfList == null ){
+            fail("The list of bookshelf cannot be null");
+        }
     }
 
     @Test
-    void makeTags() {
+    void makeTags() throws SQLException {
+        List <Tag> tagList =dbtest.getTags ();
+        if (tagList == null ){
+            fail("The list of tags cannot be null");
+        }
     }
 
     void tearDown() {
